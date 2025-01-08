3dd4e92280b4b4d108a70c4b51cd127e2f7c49e2
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2025-01-08 12:52:59 +0200

    feat: add new Sports icons [MDS-1477] (#25)

--- src/icons/ArrowsUp.tsx
+++ src/icons/ArrowsUp.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
-const ArrowsUp = (props: React.SVGProps<SVGSVGElement>) => <svg width="1em" height="1em" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M15.9986 26.0036L15.9948 5.99647M15.9948 5.99647L10.6083 11.383M15.9948 5.99647L21.3814 11.383" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" /></svg>;
+const ArrowsUp = (props: React.SVGProps<SVGSVGElement>) => <svg width="1em" height="1em" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M15.9986 26.0036L15.9948 5.99644M15.9948 5.99644L10.6083 11.3829M15.9948 5.99644L21.3814 11.3829" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" /></svg>;
 export default ArrowsUp;
--- src/icons/GenericDownload.tsx
+++ src/icons/GenericDownload.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
-const GenericDownload = (props: React.SVGProps<SVGSVGElement>) => <svg width="1em" height="1em" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M16.0001 6.5L16.0001 21.1155M16.0001 21.1155L11.6155 16.7308M16.0001 21.1155L20.3847 16.7308M6.5 18.923V22.5769C6.5 24.1913 7.80871 25.5 9.4231 25.5H22.577C24.1914 25.5 25.5001 24.1913 25.5001 22.5769V18.923" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" /></svg>;
+const GenericDownload = (props: React.SVGProps<SVGSVGElement>) => <svg width="1em" height="1em" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M16 6.5L16 21.1155M16 21.1155L11.6154 16.7308M16 21.1155L20.3847 16.7308M6.49994 18.923V22.5769C6.49994 24.1913 7.80865 25.5 9.42303 25.5H22.577C24.1913 25.5 25.5 24.1913 25.5 22.5769V18.923" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" /></svg>;
 export default GenericDownload;
